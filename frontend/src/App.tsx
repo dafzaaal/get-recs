5c32d9b1e30ee5b7d97c4441c41e9f5318e11dc5
dafzaal <dawoodafzaal13@gmail.com>
2026-02-04 20:13:26 -0800

    changed UI, added dot effect as well as textarea

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,45 +1,45 @@
 import './App.css'
-import { easeInOut, motion, reverseEasing } from "framer-motion"
-
-type WavyTextProps = {
-  char: string
-  index: number
-}
-
-type CreateWavyTextProps = {
-  word: string
-}
-
-function WavyText(props: WavyTextProps) {
-  return (
-    <motion.span
-      initial={{ y: -25 }}
-      animate={{ y: 0 }}
-      transition={{ duration: 0.5, repeat: Infinity, ease: easeInOut, repeatType: "reverse", delay: props.index * 0.2 }}
-    >
-      {props.char}
-    </motion.span>
-  )
-}
+import { motion } from "framer-motion"
+import DotGrid from './DotGrid'
+import { useState } from 'react'
 
 
-
-function CreateWavyText(props: CreateWavyTextProps) {
-  const text: string = props.word
-  return text.split('').map((char, index) => (
-    <WavyText key={index} char={char} index={index}/>
-  ));
-}
 
 export default function App() {
+
+  const [query, setQuery] = useState('')
+
+  function handleInputStateChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
+      setQuery(event.target.value)
+      console.log(query)
+  }
+
   return (
-    <motion.div>
-        <CreateWavyText word='Welcome'/>
-        <div>
-          Welcome to <b>Get Recs!</b>!, this is a simple application leveraging the Gemini and YouTube API to
-          provide some insight on some topics of interest as well as some YouTube video reccommendations. 
-        </div>
-        <input placeholder='Enter a topic of interest...' className=' border-white rounded-lg bg-white text-black w-2xl p-3 m-10'></input>
+    <motion.div className='flex w-screen h-screen'>
+      <div id='text-area' className='w-screen flex flex-col justify-center items-center'>
+          <h1 id='header' className='font-roboto text-5xl font-extrabold text-black mb-10'>Welcome!</h1>
+            <p className='text-black w-2/3 text-center text-xl font-roboto'>
+              Welcome to <b>Get Recs!</b>, this is a dummy application leveraging the YouTube API and Gemini to take a user 
+              prompt and generate an AI response as well as reccommend a couple videos regarding the topic for the user.
+              The goal of this applicatin is to help me understand hosting a web-application on an nginx server as
+              well as learn the basics of integrating regular Playwright test cases into a GitHub Actions CI/CD pipeline.
+            </p>
+            <label className='text-black font-roboto mt-10 mb-10 font-medium text-lg'>Enter something neat you'd like to learn more about below!</label>
+            <textarea placeholder=' Type away...' className='text-white bg-black rounded-lg w-2/3 h-1/3' onChange={handleInputStateChange}></textarea>
+      </div>
+      <div id='img-area' className='w-full h-screen'>
+       <DotGrid
+          dotSize={5}
+          gap={15}
+          baseColor="#FFFFFF"
+          activeColor="#00000"
+          proximity={120}
+          shockRadius={100}
+          shockStrength={5}
+          resistance={750}
+          returnDuration={1.5}
+       />
+      </div>
     </motion.div>
   )
 }
