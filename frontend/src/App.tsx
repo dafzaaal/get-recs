50082f55791a3a191bc3f85ecca563e1197e7140
dafzaal <dawoodafzaal13@gmail.com>
2026-02-04 22:46:20 -0800

    added button for learning as well as hover animation as well as an animation for the web page header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
 import './App.css'
 import { motion } from "framer-motion"
 import DotGrid from './DotGrid'
+import SplitText from './SplitText'
 import { useState } from 'react'
+
 
 
 
@@ -14,10 +16,27 @@ export default function App() {
       console.log(query)
   }
 
+  const handleAnimationComplete = () => {
+    console.log('All letters have animated!');
+  };
+
   return (
     <motion.div className='flex w-screen h-screen'>
       <div id='text-area' className='w-screen flex flex-col justify-center items-center'>
-          <h1 id='header' className='font-roboto text-5xl font-extrabold text-black mb-10'>Welcome!</h1>
+         <SplitText
+            text="Welcome!"
+            className="text-8xl font-roboto font-extrabold text-center mb-5 text-black"
+            delay={50}
+            duration={3}
+            ease="power3.out"
+            splitType="chars"
+            from={{ opacity: 0, y: 40 }}
+            to={{ opacity: 1, y: 0 }}
+            threshold={0.1}
+            rootMargin="-100px"
+            textAlign="center"
+            onLetterAnimationComplete={handleAnimationComplete}
+          />
             <p className='text-black w-2/3 text-center text-xl font-roboto'>
               Welcome to <b>Get Recs!</b>, this is a dummy application leveraging the YouTube API and Gemini to take a user 
               prompt and generate an AI response as well as reccommend a couple videos regarding the topic for the user.
@@ -26,18 +45,26 @@ export default function App() {
             </p>
             <label className='text-black font-roboto mt-10 mb-10 font-medium text-lg'>Enter something neat you'd like to learn more about below!</label>
             <textarea placeholder=' Type away...' className='text-white bg-black rounded-lg w-2/3 h-1/3' onChange={handleInputStateChange}></textarea>
+            <motion.button 
+              className='shadow-2xl mt-5 bg-amber-100'
+              initial={false}
+              whileHover={{ scale: 1.1 }}
+              transition={{ duration: 0.35 }}
+            
+            >
+              Start learning...</motion.button>
       </div>
       <div id='img-area' className='w-full h-screen'>
        <DotGrid
-          dotSize={5}
+          dotSize={13}
           gap={15}
           baseColor="#FFFFFF"
           activeColor="#00000"
           proximity={120}
-          shockRadius={100}
-          shockStrength={5}
+          shockRadius={250}
+          shockStrength={10}
           resistance={750}
-          returnDuration={1.5}
+          returnDuration={1}
        />
       </div>
     </motion.div>
